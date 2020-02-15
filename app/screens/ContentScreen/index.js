418b07fd06745fa7ae9c9113069d0e5464cd5804
Oğuzhan İNCE <oguzhan824@gmail.com>
2020-02-15 12:31:20 +0300

    content screen finished

--- app/screens/ContentScreen/index.js
+++ app/screens/ContentScreen/index.js
@@ -1,20 +1,58 @@
 import * as React from 'react';
-import {StyleSheet, View, ScrollView, Text} from 'react-native';
-import {Colors, Images} from '../../theme';
+import {StyleSheet, View, ScrollView, Text, Image} from 'react-native';
+import Author from '../../components/Author';
+import {Colors, Images, Fonts, Helpers} from '../../theme';
 
-const ContentScreen = () => {
+const ContentScreen = ({route, navigation}) => {
+  const {title, content, image} = route.params;
+  const firstContent = content.firstContent.paragraph;
+  const secondContent = content.secondContent.paragraph;
+  const thirdContent = content.thirdContent.paragraph;
+  navigation.setOptions({title: title});
   return (
-    <View style={[styles.container]}>
-      <Text> Hello </Text>
-    </View>
+    <ScrollView>
+      <View style={[styles.container, Helpers.textLeft]}>
+        <Image
+          source={Images[image]}
+          resizeMode={'contain'}
+          style={styles.contentImage}
+        />
+        <Author />
+        <Text style={[Fonts.h1, styles.title]}>
+          {content.firstContent.title}
+        </Text>
+        <Text style={(Helpers.textLeft, styles.paragraph)}>{firstContent}</Text>
+        <Text style={[Fonts.h1, styles.title]}>
+          {content.secondContent.title}
+        </Text>
+        <Text style={(Helpers.textLeft, styles.paragraph)}>
+          {secondContent}
+        </Text>
+        <Text style={[Fonts.h1, styles.title]}>
+          {content.thirdContent.title}
+        </Text>
+        <Text style={(Helpers.textLeft, styles.paragraph)}>{thirdContent}</Text>
+      </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.white,
-    alignItems: 'center',
     flex: 1,
+    padding: 18,
+  },
+  contentImage: {
+    width: 'auto',
+    height: 250,
+  },
+  title: {
+    marginVertical: 10,
+  },
+  paragraph: {
+    lineHeight: 22,
+    fontFamily: 'Roboto-Regular',
   },
 });
 
